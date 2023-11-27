6187d5681ee622ea1e8be443149ca5bb2d4bbeca
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-11-27 15:17:00 +0200

    setup app configuration

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,8 +1,23 @@
+import os
+
 from flask import Flask
 
 
-def create_app():
-    app = Flask(__name__)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+
+    if test_config:
+        app.config.from_mapping(test_config)
+    else:
+        app.config.from_pyfile("config.py", silent=True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     @app.route("/")
     def index():
