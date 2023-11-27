b5415a7367f12e415ff37840a5d5dd910d7c20f0
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-11-27 20:29:53 +0200

    implement login route with get and post

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, redirect, render_template, request, url_for
-from werkzeug.security import generate_password_hash
+from flask import Blueprint, redirect, render_template, request, session, url_for
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
 
@@ -36,6 +36,27 @@ def register():
     return render_template("auth/register.jinja", error=error)
 
 
-@bp.route("/login")
+@bp.route("/login", methods=("GET", "POST"))
 def login():
-    return render_template("auth/login.jinja")
+    error = None
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        db = get_db()
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,)
+        ).fetchone()
+
+        if not user:
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
+
+        if not error:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+
+    return render_template("auth/login.jinja", error=error)
