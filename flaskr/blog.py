bfc07b8eba7d2aecfbc551d20d09aaccf20978d5
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-11-28 20:39:34 +0200

    create route for new post

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, g, redirect, render_template, request, url_for
 
+from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint("blog", __name__)
@@ -14,3 +15,29 @@ def index():
         " ORDER BY created_at DESC"
     ).fetchall()
     return render_template("blog/index.jinja", posts=posts)
+
+
+@bp.route("/create", methods=("GET", "POST"))
+@login_required
+def create():
+    error = None
+
+    if request.method == "POST":
+        title = request.form.get("title")
+        body = request.form.get("body")
+
+        if not title:
+            error = "Title is required."
+        elif not body:
+            error = "Body is required."
+
+        if not error:
+            db = get_db()
+            db.execute(
+                "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
+                (title, body, g.user["id"]),
+            )
+            db.commit()
+            return redirect(url_for("blog.index"))
+
+    return render_template("blog/create.jinja", error=error)
